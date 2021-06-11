21887125f990d055779c3347dc81664189e7386f
aiperi-s <aiperi2211@gmail.com>
2021-06-11 20:02:59 +0000

    added owner

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -7,6 +7,7 @@ def hello_world():
     return jsonify({
         'message': 'hello world',
         'environment': os.environ.get('ENVIRONMENT'),
+        'owner': 'aiperi-s'
         'namespace': os.environ.get('NAMESPACE')
     })
     
