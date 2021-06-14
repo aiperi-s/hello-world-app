2dcfa6880cc16bdc3fff468ef5a68f748ba19c61
aiperi-s <aiperi2211@gmail.com>
2021-06-14 17:38:56 +0000

    added variables

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,39 @@
+module "helm_deploy" {
+  source = "fuchicorp/chart/helm"
+  deployment_name           = "${var.deployment_name}"
+  deployment_enviroment     = "${var.deployment_enviroment}"
+  deployment_endpoint       = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+  deployment_path           = "hello-worl"
+  template_custom_vars = { 
+    deployment_image        = "${var.deployment_image}" 
+  }
+}
+
+variable "deployment_name" {
+  default = "hello-world"
+  description = "- (Optional) The name of the deployment"
+}
+
+variable "deployment_enviroment" {
+  default = "prod"
+  description = "- (Optional) The namespace to deploy the helm chart"
+}
+
+variable "deployment_endpoint" {
+  type = "map"
+  default = {
+    dev   = "dev.hello"
+    qa    = "qa.hello"
+    prod  = "hello"
+    stage = "stage.hello"
+  }
+  description = "- (Optional) The endpoint of the deployment."
+}
+variable "deployment_image" {
+  default = "docker.aiperisarinzhi.com/hello-world-stage:6f63e63"
+  description =" - (Optional) The docker image of the deployment"
+}
+
+variable "google_domain_name"{
+  default = "aiperisarinzhi.com"
+}
