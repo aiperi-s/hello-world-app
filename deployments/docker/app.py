f21e09a70eb48f065b47fc888825be0f20b7462f
aiperi-s <aiperi2211@gmail.com>
2021-06-17 19:55:15 +0000

    added aiperi-s page

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
+        'message': 'Hello I am a developer my name is Aiperi S',
         'environment': os.environ.get('ENVIRONMENT'),
         'owner': 'aiperi-s'
         'namespace': os.environ.get('NAMESPACE')
     })
     
-@app.route('/soon')
+@app.route('/aiperi-s')
 def comming_soon():
     return jsonify({
-        'message': 'This is comming soon page!!'
+        'message': 'This is my page'
     })
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
